9cce70531fe8635b8b5375b36bbd0079cbef065a
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 19:01:17 +0530

    Add files via upload

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    public class Drawing
+    {
+        public virtual double Area()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Drawing
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Radius = 5;
+        }
+        public override double Area()
+        {
+            return (3.14) * Math.Pow(Radius, 2);
+        }
+    }
+    public class Rectangle : Drawing
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public Rectangle()
+        {
+            Height = 5.3;
+            Width = 3.4;
+        }
+        public override double Area()
+        {
+            return Height * Width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Drawing circle = new Circle();
+            Console.WriteLine("Area :" + circle.Area());
+
+            Drawing rectangle = new Rectangle();
+            Console.WriteLine("Area :" + rectangle.Area());
+        }
+    }
+}
