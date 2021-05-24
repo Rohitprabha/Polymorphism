df70ee79f3130961a63f3c5602740dcb30bbcbc7
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 19:04:42 +0530

    Add files via upload

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public static void Add(int x, int y, int z)
+        {
+            Console.WriteLine(x + y + z);
+        }
+        public static void Add(double x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+        static void Main(string[] args)
+        {
+            Add(2, 3);
+            Add(2, 3, 4);
+            Add(3.2, 2.3);
+            
+        }
+    }
+}
