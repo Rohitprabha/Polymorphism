c296de442dd3083605019e3dd88f95930a26d922
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 19:10:14 +0530

    Add files via upload

---
+++ Method Overriding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Runtime_Polymorphism
+{
+    class Base
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("Base class print");
+        }
+    }
+    class Derived : Base
+    {
+        public override void print()
+        {
+            base.print();
+            Console.WriteLine("Derived class print");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived();
+            d.print();
+        }
+    }
+}
